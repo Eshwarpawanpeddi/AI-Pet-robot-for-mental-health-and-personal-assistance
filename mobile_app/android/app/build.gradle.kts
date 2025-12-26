419cc56ef437fa3370b083466532b324840ba25a
str626 <str@nutrixcorp.com>
2025-12-27 02:15:36 +0530

    semi final changes

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -1,27 +1,30 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
-    // This line links Flutter to your Android code and fixes the compilation errors
-    id "dev.flutter.flutter-gradle-plugin"
+    id("com.android.application")
+    id("kotlin-android")
+    // This line MUST have parentheses to link the Flutter SDK
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
+    // This must match your package name exactly
     namespace = "com.petrobot.ai_pet_robot"
     compileSdk = 34
-    
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+
     defaultConfig {
         applicationId = "com.petrobot.ai_pet_robot"
-        minSdk = flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion 
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
-    }
-
-    buildTypes {
-        release {
-            // Using debug keys for development release
-            signingConfig = signingConfigs.debug
-        }
     }
 }
 
